6fdef2126364dde8453cf19bfc7dcd29132c2447
NaNdalal-dev <dnandalal7@gmail.com>
2021-07-27 23:55:16 +0530

    commit 4

--- ContactData/PhoneNumber.java
+++ ContactData/PhoneNumber.java
@@ -1,6 +1,6 @@
+package ContactData;
 import java.util.ArrayList;
 import java.util.List;
-package ContactData;
 public  class PhoneNumber{
 	String label[] = new String[255];
 	List phoneNumber = new ArrayList();
