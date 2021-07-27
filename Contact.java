0f74c30f149a2c23fc4ca42bf126f272ee25f53e
NaNdalal-dev <dnandalal7@gmail.com>
2021-07-27 23:59:59 +0530

    Commit 5

--- Contact.java
+++ Contact.java
@@ -3,5 +3,7 @@ import ContactData.Address;
 class Contact{
 	String name[] = new String[255];
 	String organization[] = new String[255];
+	PhoneNumber pn = new PhoneNumber();
+	Address ad = new Address();
 
 }
--- ContactData/PhoneNumber.java
+++ ContactData/PhoneNumber.java
@@ -3,5 +3,5 @@ import java.util.ArrayList;
 import java.util.List;
 public  class PhoneNumber{
 	String label[] = new String[255];
-	List phoneNumber = new ArrayList();
+	List<Integer> phoneNumber = new ArrayList<Integer>();
 }
